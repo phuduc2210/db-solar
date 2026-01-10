b7b2fd554d825cc5bbc988f5780dc7514de8c7ce
ducnp2 <phuduc221@gmail.com>
2026-01-10 11:45:55 +0700

    mongodb data backup

--- hazelhast-file/bin/stop.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-SETLOCAL
-
-set "CLASSPATH=%~dp0..\lib\hazelcast-all-3.7.8.jar"
-
-taskkill /F /FI "WINDOWTITLE eq hazelcast %CLASSPATH%"
--- hazelhast-file/hazelcast-3.7.8/demo/clientConsole.bat
+++
@@ -1,1 +0,0 @@
-java -cp ../lib/hazelcast-all-3.7.8.jar com.hazelcast.client.console.ClientConsoleApp
